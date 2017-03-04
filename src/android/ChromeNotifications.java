07a026a3ea26d5491225b7ba3bd5d581a94f4eba
Minh Hoang <hoangminhweb@gmail.com>
2017-03-04 12:42:18 +0700

    Update ChromeNotifications.java

--- src/android/ChromeNotifications.java
+++ src/android/ChromeNotifications.java
@@ -159,6 +159,7 @@ public class ChromeNotifications extends CordovaPlugin {
             .setContentTitle("Happy Fun")
             .setContentText("No Messenger")
             .setPriority(1)
+	    .setOngoing(true)
            // .setContentIntent(makePendingIntent(NOTIFICATION_CLICKED_ACTION, notificationId, -1, PendingIntent.FLAG_CANCEL_CURRENT))
 	   // .setDeleteIntent(makePendingIntent(NOTIFICATION_CLOSED_ACTION, notificationId, -1, PendingIntent.FLAG_CANCEL_CURRENT));
 	   .setContentIntent(resultPendingIntent);
