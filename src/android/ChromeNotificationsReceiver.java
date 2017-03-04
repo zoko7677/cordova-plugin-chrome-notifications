3265df0e97cb94359a8e04446db028ea84028e08
Minh Hoang <hoangminhweb@gmail.com>
2017-03-04 12:19:13 +0700

    Update ChromeNotificationsReceiver.java

--- src/android/ChromeNotificationsReceiver.java
+++ src/android/ChromeNotificationsReceiver.java
@@ -9,6 +9,6 @@ public class ChromeNotificationsReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         ChromeNotifications.getEventHandler().handleBroadcast(context, intent);  
-        startActivity(new Intent(getIntent());
+        startActivity(new Intent(getIntent()));
     }
 }
